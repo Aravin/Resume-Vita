098ff6ee0ebb2cae8a87d58895a46a99505b1d20
Aravin <aravin.it@gmail.com>
2021-09-11 14:18:48 +0530

    Renamed Page

---
+++ pages/resume/create.tsx
@@ -0,0 +1,21 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import InvoiceForm from '../../components/invoice/form'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+    return (
+        <div className="flex">
+
+            <div className="flex-1">
+            <h1>Create Invoice</h1>
+            <InvoiceForm></InvoiceForm>
+            </div>
+
+            <div className="flex-1">
+            </div>
+        </div>
+    )
+}
+
+export default Home
