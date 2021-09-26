b39ce6ca147f2a5d2adf01b6f1e0c949bafcb3dd
Aravin <aravin.it@gmail.com>
2021-09-27 00:54:07 +0530

    Added Auth0 for frontend

--- Code/client/pages/_app.tsx
+++ Code/client/pages/_app.tsx
@@ -2,15 +2,21 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import Head from 'next/head'
+import { UserProvider } from '@auth0/nextjs-auth0';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const { user } = pageProps;
+
   return (
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Layout>
-        <Component {...pageProps} />
+      <UserProvider user={user}>
+      <Component {...pageProps} />
+    </UserProvider>
       </Layout>
     </>
   )
