a47620a6b61e50e5f1ada81650580b8bd01fd07b
Aravin <aravin.it@gmail.com>
2021-09-26 10:38:52 +0530

    Added DB Connection

--- Code/api/src/config.ts
+++ Code/api/src/config.ts
@@ -3,4 +3,9 @@ dotenv.config();
 
 export const appConfig = {
     port: process.env.PORT,
+    db: {
+        name: process.env.DB_NAME,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASS,
+    }
 }
