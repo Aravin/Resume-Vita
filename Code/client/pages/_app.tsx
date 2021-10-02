d0d0cb3f1a2be4472ebd3b75fc4a58ab7ba2549e
Aravin <aravin.it@gmail.com>
2021-10-02 17:31:40 +0530

    Added Loader to the Application

--- Code/client/pages/_app.tsx
+++ Code/client/pages/_app.tsx
@@ -3,6 +3,14 @@ import type { AppProps } from 'next/app';
 import Layout from '../components/layout';
 import Head from 'next/head'
 import { UserProvider } from '@auth0/nextjs-auth0';
+import Router from 'next/router';
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
+
+//Binding events. 
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());  
 
 function MyApp({ Component, pageProps }: AppProps) {
 
