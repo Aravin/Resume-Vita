46fd205ae8536617505bb4dd7bee4d3b3efb4c36
Aravin <aravin.it@gmail.com>
2021-10-11 22:37:26 +0530

    Fixed image overflow in homepage

--- Code/client/pages/index.tsx
+++ Code/client/pages/index.tsx
@@ -6,22 +6,24 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="hero min-h-screen bg-base-200">
-        <div className="flex-col hero-content lg:flex-row-reverse">
-          <Image src="/resume_undraw.svg" alt="Resume Tree Hero Image" className="max-w-md" />
+        <div className="flex-col hero-content lg:flex-row">
+          <div className="sm:hidden lg:block max-w-md">
+          <Image src="/resume_undraw.svg" alt="Resume Tree Hero Image" width={600} height={600}  />
+          </div>
           <div>
-          <h1 className="mb-5 text-5xl font-bold">
-          Resume Tree (Alpha)
-          </h1>
-          <p className="mb-5">
-          Generate a resume, download as PDF, download as Word and share directly to your friends or recruiter. This is free and open source service. 
-          </p>
-          <button className="btn btn-primary">Get Started</button>
+            <h1 className="mb-5 text-5xl font-bold">
+              Resume Tree (Alpha)
+            </h1>
+            <p className="mb-5">
+              Generate a resume, download as PDF, download as Word and share directly to your friends or recruiter. This is free and open source service.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
           </div>
         </div>
       </div>
 
       {/* Feature Section */}
-      
+
     </>
   )
 }
