0126cbd9baf957f0bd3d78723917dc0797c87c00
Aravin <aravin.it@gmail.com>
2021-11-02 01:22:22 +0530

    Updated useToggle

--- Code/client/hooks/useToggle.tsx
+++ Code/client/hooks/useToggle.tsx
@@ -7,5 +7,6 @@ export const useToggle = (initialState: boolean = false): [boolean, any] => {
     // Define and memorize toggler function in case we pass down the comopnent,
     // This function change the boolean value to it's opposite value
     const toggle = useCallback((): void => setState(state => !state), []);
+
     return [state, toggle]
 }
