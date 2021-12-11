7f771f8f880978a0eac9f8004f703be3b3ed9c2f
Aravin <aravin.it@gmail.com>
2021-12-11 21:36:35 +0530

    Updated All the Packages

--- Code/client/next.config.js
+++ Code/client/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  swcMinify: true,
   reactStrictMode: true,
   images: {
     domains: ['localhost', 'lh3.googleusercontent.com', 's.gravatar.com', 'resume-vita.s3.ap-south-1.amazonaws.com'],
