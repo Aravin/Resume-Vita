2be8fd98545c21fb274b4d59012d0e2ac0c4999f
Aravin <4869265+Aravin@users.noreply.github.com>
2022-09-23 10:29:19 +0530

    Removed unused reference

--- Code/api/src/server.ts
+++ Code/api/src/server.ts
@@ -4,15 +4,13 @@ import cors from 'cors';
 import { appConfig } from './config';
 import { router } from './router';
 import { db } from './helpers/db';
-import path from 'path';
-import { browser } from './helpers/browser';
 
 // initialize the express app
 const app = express();
 
 // add middleware
 app
-    .use(cors({origin: '*'}))
+    .use(cors())
     .use('/public', express.static(__dirname + '/public'))
     .use(async (req, res, next) => {
         // initialize the db
