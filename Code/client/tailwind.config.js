4b0e0fc28a73bfeb4cff4958a129dd10baf8fd54
Aravind A <4869265+Aravin@users.noreply.github.com>
2025-01-27 20:12:22 +0530

    removed @tailwindcss/line-clamp

--- Code/client/tailwind.config.js
+++ Code/client/tailwind.config.js
@@ -9,7 +9,6 @@ module.exports = {
   plugins: [
     require('@tailwindcss/typography'),
     require('@tailwindcss/forms'),
-    require('@tailwindcss/line-clamp'),
     require('daisyui'),
   ],
   daisyui: {
