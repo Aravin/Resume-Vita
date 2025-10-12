b6afc71190714d06207335b416f4133fe65e3e55
Aravind A <4869265+Aravin@users.noreply.github.com>
2025-10-12 13:59:24 +0530

    feat: add environment configuration for API with database and AWS settings

--- Code/api/src/config.ts
+++ Code/api/src/config.ts
@@ -9,11 +9,7 @@ export const appConfig = {
         password: process.env.DB_PASS,
     },
     aws: {
-        user: process.env.AWS_USER,
-        accessKey: process.env.AWS_ACCESS_KEY_ID,
-        secret: process.env.AWS_SECRET_ACCESS_KEY,
         storageBucket: process.env.AWS_S3_BUCKET,
         region: process.env.AWS_S3_REGION,
-        bucketURL: process.env.AWS_S3_BUCKET_URL,
     }
 }
