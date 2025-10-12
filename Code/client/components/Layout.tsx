2d0ad867bdc316fd1946731c923f0ae50299f0bc
Aravind A <4869265+Aravin@users.noreply.github.com>
2025-10-12 20:35:28 +0530

    Revert "feat: add Banner component for backend maintenance notifications"
    This reverts commit 0e39e13a79e113a21f003da9764cac1885490c59.

--- Code/client/components/Layout.tsx
+++ Code/client/components/Layout.tsx
@@ -1,12 +1,10 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Banner from "./Banner";
 
 export default function Layout({ children }: any) {
   return (
     <>
       <Navbar />
-      <Banner />
       {/* todo optimize */}
       <main className="p-4 md:p-8 lg:p-12 min-h-[60vh]">{children}</main>
       <Footer />
