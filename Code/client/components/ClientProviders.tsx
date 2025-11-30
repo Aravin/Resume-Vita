fc972aaefe93c97559a0b59771e63814293a94ac
Aravind A <4869265+Aravin@users.noreply.github.com>
2025-11-30 19:33:31 +0530

    feat: enable Auth0 UserProvider and populate SafeUserContext with actual user data

--- Code/client/components/ClientProviders.tsx
+++ Code/client/components/ClientProviders.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-// import { UserProvider } from "@auth0/nextjs-auth0/client";
+import { UserProvider, useUser } from "@auth0/nextjs-auth0/client";
 import { AppProgressBar as ProgressBar } from "next-nprogress-bar";
 import { SafeUserContext } from "../hooks/useSafeUser";
+import React from "react";
+
+const SafeUserProvider = ({ children }: { children: React.ReactNode }) => {
+  const { user, error, isLoading } = useUser();
+  
+  return (
+    <SafeUserContext.Provider value={{ user, error, isLoading }}>
+      {children}
+    </SafeUserContext.Provider>
+  );
+};
 
 export default function ClientProviders({ children }: { children: React.ReactNode }) {
-  // Temporarily disable UserProvider to prevent SSR errors
   return (
-    <SafeUserContext.Provider value={{ user: null, error: null, isLoading: false }}>
-      {children}
-      <ProgressBar />
-    </SafeUserContext.Provider>
+    <UserProvider>
+      <SafeUserProvider>
+        {children}
+        {/* <ProgressBar /> */}
+      </SafeUserProvider>
+    </UserProvider>
   );
 }
